2d4b5fca45651bcfa088751b488a892c776ed1ef
Janakitti <janakittiratana@gmail.com>
2020-10-23 18:14:04 -0400

    Implement step movement

---
+++ SecretsGame/Assets/Scripts/PlayerContoller.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContoller : MonoBehaviour
+{
+    public Vector3 moveDirection = Vector3.zero;
+
+    private Vector3 nextPos;
+    public StepController stepController;
+    void Start()
+    {
+        nextPos = transform.position;
+    }
+
+    void Update()
+    {
+        if (stepController.isDoneMoving)
+        {
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                stepController.SetNextLocation(transform.TransformPoint(Vector3.forward));
+            } else if (Input.GetKeyDown(KeyCode.S))
+            {
+                stepController.SetNextLocation(transform.TransformPoint(Vector3.back));
+            } else if (Input.GetKeyDown(KeyCode.A))
+            {
+                stepController.SetNextLocation(transform.TransformPoint(Vector3.left));
+            } else if (Input.GetKeyDown(KeyCode.D))
+            {
+                stepController.SetNextLocation(transform.TransformPoint(Vector3.right));
+            }
+        }
+
+
+    }
+    void Step(string dir)
+    {
+        if (dir == "North")
+        {
+            stepController.SetNextLocation(transform.TransformPoint(Vector3.forward));
+            //transform.translate(1f, 0f, 0f);
+        }
+        
+    }
+}
+
+
+
---
+++ SecretsGame/Assets/Scripts/StepController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StepController : MonoBehaviour
+{
+    public float speed = 4f;
+    public bool isDoneMoving;
+
+    private Vector3 nextLocation;
+
+
+    void Start()
+    {
+        SetNextLocation(gameObject.transform.position);
+        isDoneMoving = true;
+    }
+
+    void Update()
+    {
+        if (gameObject.transform.position != nextLocation)
+        {
+            isDoneMoving = false;
+            SlideStep();
+        } else
+        {
+            isDoneMoving = true;
+        }
+    }
+
+    void SlideStep()
+    {
+        gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, nextLocation, speed * Time.deltaTime);
+    }
+
+    public void SetNextLocation(Vector3 location)
+    {
+        nextLocation = location;
+    }
+}
