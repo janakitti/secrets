9a2e5bbc902a7d552b32227b4dfae0f201432339
Janakitti <janakittiratana@gmail.com>
2020-10-29 22:18:20 -0400

    Create camera animation

---
+++ SecretsGame/Assets/Scripts/MenuCameraAnimation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCameraAnimation : MonoBehaviour
+{
+    public Animator anim;
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ SecretsGame/Assets/Scripts/MenuCubeClick.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCubeClick : MonoBehaviour
+{
+
+    public MenuCameraAnimation camera;
+    private void Start()
+    {
+
+    }
+    void OnMouseDown()
+    {
+        camera.anim.SetTrigger("ClickCube");
+    }
+}
